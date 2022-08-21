8e46ae3868ee05ac53a8164b8c28144e456ef57d
Modit Parmar <modit987@gmail.com>
2022-08-21 13:59:01 +0530

    added new file.

---
+++ Udemy2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Udemy2 {
+   
+   public static void main(String[] args) {
+
+        int x;
+
+        Scanner sc = new Scanner(System.in);
+        x = sc.nextInt();
+        sc.close();
+
+        System.out.println(sumDigits(x));
+
+   }
+   
+    public static int sumDigits(int x){
+        
+        int temp,sum=0;
+
+        if (x>=10){
+            while(x!=0){
+                x = x%10;
+                temp = x;
+                sum += temp;
+                x = x/10;
+            }
+            return sum;
+        }
+
+        else
+        return -1;
+    }
+}
