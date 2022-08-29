8337d80ab743b6219fa948ed84fa493594e8395a
Modit Parmar <modit987@gmail.com>
2022-08-29 18:29:05 +0530

    Composition topic covered.

---
+++ Composition/src/Dimensions.java
@@ -0,0 +1,38 @@
+package Composition.src;
+public class Dimensions{
+    private int height;
+    private int width;
+    private int length;
+
+
+    public Dimensions(int height, int width, int length) {
+        this.height = height;
+        this.width = width;
+        this.length = length;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLength() {
+        return this.length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+}
