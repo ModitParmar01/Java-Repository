c4f77bc2529a55a050b68192afcc80457727187d
Modit <86144424+ModitParmar01@users.noreply.github.com>
2022-09-22 18:45:58 +0530

    Life Sorted!

--- Arrays/SortedArrays.java
+++ Arrays/SortedArrays.java
@@ -3,7 +3,7 @@ public class SortedArrays {
 
 /*arrays sorted in descending*/
     public static void main(String[] args) {
-        int[] arr = {2,3,4,5,6};
+        int[] arr = {2,3,4,5,6,23,24,26};
         int[] soretedArray = sort(arr);
         for(int i=0; i<arr.length; i++){
             System.out.println(soretedArray[i]);
