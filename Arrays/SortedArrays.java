5d149364874e701f3c216851eeb5d4db84e9acba
Modit Parmar <modit987@gmail.com>
2022-09-22 18:36:11 +0530

    life sorted!

---
+++ Arrays/SortedArrays.java
@@ -0,0 +1,27 @@
+package Arrays;
+public class SortedArrays {
+
+
+    public static void main(String[] args) {
+        int[] arr = {2,3,4,5,6};
+        int[] soretedArray = sort(arr);
+        for(int i=0; i<arr.length; i++){
+            System.out.println(soretedArray[i]);
+        }
+    }
+
+
+    public static int[] sort(int[] arr){
+        int temp=0;
+        for(int j=0; j<arr.length-1; j++){
+        for(int i=0; i<arr.length-1;i++){
+            if(arr[i]<arr[i+1]){
+                temp = arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+            }
+        }
+    }
+        return arr;
+    }
+}
