cd3d274be5a9f45f0b5e6dbdbed90074ab771b85
Modit Parmar <modit987@gmail.com>
2022-10-17 13:38:18 +0530

    added method file

---
+++ RefrenceTypeValuedType/src/methodArray.java
@@ -0,0 +1,7 @@
+package RefrenceTypeValuedType.src;
+public class methodArray {
+    public static void modifyArray(int[] array){
+        array[0] = 69;
+        array = new int[] {1,2,3,4,5};
+    }
+}
