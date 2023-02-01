65492cbcd699f0626844503126e05a9aa223046e
Modit Parmar <modit987@gmail.com>
2023-02-01 11:40:26 +0530

    added RecursionFactorial

---
+++ RecursionFactorial.java
@@ -0,0 +1,16 @@
+public class RecursionFactorial {
+    
+    public static int fact(int n){
+        if (n==0){
+            return 1;
+        }
+
+        else
+        return fact(n-1)*n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+    }
+
+}
