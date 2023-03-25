605b4b4c14f140a72e84c0947313b300e92374e4
Modit Parmar <modit987@gmail.com>
2023-03-25 16:58:38 +0530

    linear searching added

--- DSA/ArrayLinearSearching.java
+++ DSA/ArrayLinearSearching.java
@@ -1,4 +1,18 @@
 package DSA;
 public class ArrayLinearSearching {
-    
+    public static int search(int key, int[] arr){
+
+        for(int i=0; i<arr.length;i++){
+            if(key==arr[i]){
+                return i;
+            }
+        }
+
+        return-1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        System.out.println(search(7, arr));
+    }
 }
